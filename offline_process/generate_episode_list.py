b55a89d421dd676b15112baaeb3190aa352d3f47
oka <pn11@users.noreply.github.com>
2022-05-04 04:08:31 +0900

    change json format

--- offline_process/generate_episode_list.py
+++ offline_process/generate_episode_list.py
@@ -13,17 +13,18 @@ def parse(feed):
     return sorted(episodes, key=lambda x: x['published'])
 
 
-
 def main(args=None):
-    episodes = {}
+    episodes = []
     shows = json.load(open('shows.json'))
     for show in shows:
         try:
-            parsed = podcastparser.parse(show['rss'], urllib.request.urlopen(show['rss'], timeout=1))
-            episodes[show['id']] = {
-                'rss': show['rss'],
-                'episodes': parsed['episodes']
-            }
+            parsed = podcastparser.parse(show['rss'], urllib.request.urlopen(show['rss'], timeout=5))
+            show_title = parsed['title']
+            for episode in parsed['episodes']:
+                episode['rss'] = show['rss']
+                episode['show_id'] = show['id']
+                episode['show_title'] = show_title
+                episodes.append(episode)
         except socket.timeout:
             print('Timeout.', show)
         except podcastparser.FeedParseError:
@@ -33,7 +34,7 @@ def main(args=None):
         except urllib.error.URLError:
             print('HTTPError', show)
 
-    json.dump(episodes, open('episodes.json', 'w'), ensure_ascii=False, indent=2)
+    json.dump(episodes, open('../src/assets/episodes.json', 'w'), ensure_ascii=False)
 
 
 if __name__ == '__main__':
